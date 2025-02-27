2582d047e7ee4190a73e6f3d21d2720378d4ce4e
pardana <aditria.pardana@yahoo.co.id>
2025-02-27 14:48:39 +0700

    add: service card

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero";
+import Services from "./components/services";
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <Hero />
+      <Services />
     </main>
   );
 };
---
+++ src/components/services.tsx
@@ -0,0 +1,46 @@
+import { BiSupport } from "react-icons/bi";
+import { LiaShippingFastSolid } from "react-icons/lia";
+import { RiRefund2Line, RiSecurePaymentLine } from "react-icons/ri";
+import ServiceCard from "./servicecard";
+
+const servicesData = [
+  {
+    icon: <LiaShippingFastSolid size={36} />,
+    title: "Free Shipping",
+    desc: "Free Shipping for all IDN Order",
+  },
+  {
+    icon: <BiSupport size={36} />,
+    title: "Support 24/7",
+    desc: "We support 24h a day",
+  },
+  {
+    icon: <RiRefund2Line size={36} />,
+    title: "100% Money Back",
+    desc: "You have 7 Days to Return",
+  },
+  {
+    icon: <RiSecurePaymentLine size={36} />,
+    title: "Secure Payment",
+    desc: "We ensure secure payment",
+  },
+];
+
+const Services = () => {
+  return (
+    <section className="container mt-[100px]">
+      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {servicesData.map((item) => (
+          <ServiceCard
+            key={item.title}
+            icon={item.icon}
+            title={item.title}
+            desc={item.desc}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
